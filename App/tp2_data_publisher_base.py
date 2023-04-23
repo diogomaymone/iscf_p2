f41c4d804af7b6f86098df663455137da2273390
Diogo Maymone <d.maymone@campus.fct.unl.pt>
2023-04-23 22:59:36 +0100

    ,

--- App/tp2_data_publisher_base.py
+++ App/tp2_data_publisher_base.py
@@ -1,6 +1,6 @@
+import os
 import paho.mqtt.publish as pub
 import numpy as np
-import os
 import time
 
 def generate(median=90, err=10, outlier_err=30, size=1000, outlier_size=10):
@@ -19,22 +19,17 @@ def generate(median=90, err=10, outlier_err=30, size=1000, outlier_size=10):
     return data
 
 if __name__ == '__main__':
-    # retrieve the environment variable values for the mqtt broker and the desired rate for the publisher
-    broker = os.environ.get('MQTT_BROKER')
-    rate = float(os.environ.get('PUBLISH_RATE'))
-    topic = os.environ.get('MQTT_TOPIC')
+    broker = os.environ.get('MQTT_BROKER', 'localhost')
+    rate = int(os.environ.get('RATE', 1))
+    topic = os.environ.get('TOPIC', 'data')
 
-    # use the generate function to create a pool of values for the publisher
     data = generate()
 
-    # publish a msg with a value randomly sampled from the data array.
-    # make it so that there's a 10% chance the value sent is null to emulate a sensor failure
-    # you should filter these null values in Node Red
     while True:
         value = np.random.choice(data)
-        if np.random.rand() < 0.1:
+        if np.random.random() < 0.1:
             value = None
         if value is not None:
-            payload = str(value)
+            payload = {'value': value}
             pub.single(topic, payload=payload, hostname=broker)
-        time.sleep(1/rate)
+        time.sleep(1 / rate)
